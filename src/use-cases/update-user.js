181c340da9308b8da88e9e63a4db77b4a6849cd1
Banilson2019 <banilsonzua98@gmail.com>
2025-10-17 02:37:03 +0100

    feat: add updateUser and getUserByEmails

---
+++ src/use-cases/update-user.js
@@ -0,0 +1,34 @@
+import bcrypt from 'bcrypt'
+import { PostgresUpdateUserRepository } from '../repositories/postegres/update-user.js'
+import { GetUserByEmailRepository } from '../repositories/postegres/get-user-by-email.js'
+import { EmailAlreadyInUserError } from '../errors/user.js'
+
+export class UpdateUserUseCase {
+    async execute(userId, updateUserParams) {
+        if (updateUserParams.email) {
+            const getUserByEmailRepository = new GetUserByEmailRepository()
+            const userWithProvidedEmail =
+                await getUserByEmailRepository.execute(updateUserParams.email)
+            if (userWithProvidedEmail) {
+                throw new EmailAlreadyInUserError(updateUserParams.email)
+            }
+        }
+
+        const user = { ...updateUserParams }
+
+        if (updateUserParams.password) {
+            const hashedPassword = await bcrypt.hash(
+                updateUserParams.password,
+                10,
+            )
+            user.password = hashedPassword
+        }
+        const postgresUpdateUserRepository = new PostgresUpdateUserRepository()
+        const updateUser = await postgresUpdateUserRepository.execute(
+            userId,
+            user,
+        )
+
+        return updateUser
+    }
+}
